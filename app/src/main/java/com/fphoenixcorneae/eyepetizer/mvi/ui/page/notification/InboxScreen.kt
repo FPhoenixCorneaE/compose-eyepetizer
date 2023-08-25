ceae5682bedaaa284dc30af76453e0885a204c40
wangkz <wangkz@infore.com>
2023-08-25 10:56:16 +0800

    feat(通知): 通知-私信：Ui

--- app/src/main/java/com/fphoenixcorneae/eyepetizer/mvi/ui/page/notification/InboxScreen.kt
+++ app/src/main/java/com/fphoenixcorneae/eyepetizer/mvi/ui/page/notification/InboxScreen.kt
@@ -1,11 +1,14 @@
 package com.fphoenixcorneae.eyepetizer.mvi.ui.page.notification
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 /**
  * @desc：通知-私信
  * @date：2023/08/24 17:26
  */
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
 @Composable
 fun InboxScreen() {
+    NotificationLoginGuide()
 }
