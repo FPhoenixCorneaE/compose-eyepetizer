cf7567eea03cf1698ccec5a017fce7720757da0f
飄然偌僊 <13104871646@163.com>
2024-04-15 22:41:34 +0800

    fix: Int.toVideoDuration()

--- app/src/main/java/com/fphoenixcorneae/eyepetizer/ext/Int.kt
+++ app/src/main/java/com/fphoenixcorneae/eyepetizer/ext/Int.kt
@@ -8,8 +8,7 @@ package com.fphoenixcorneae.eyepetizer.ext
 fun Int.toVideoDuration(): String {
     val minute = 1 * 60
     val hour = 60 * minute
-    val day = 24 * hour
-    return if (this < day) {
+    return if (this < hour) {
         String.format("%02d:%02d", this / minute, this % 60)
     } else {
         String.format("%02d:%02d:%02d", this / hour, (this % hour) / minute, this % 60)
